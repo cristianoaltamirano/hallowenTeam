409d1be921040b0eb59c098c2a68935985346514
Guillermo Rojas <ing.guillermorojas@gmail.com>
2019-06-14 16:36:35 -0300

    Player, fantasma

--- js/game/character/characterController.js
+++ js/game/character/characterController.js
@@ -12,11 +12,19 @@ halloween.CharacterController = function( app ) {
 }
 
 halloween.CharacterController.prototype.initCharacter = function(args) {
+    var that = this;
     var player = new PIXI.spine.Spine( args[1].player.spineData );
     app.stage.addChild(player);
+    
+    player.x = app.renderer.width * 0.15;
+    player.y = app.renderer.height * 0.6;
+
       if(player){
         this.view.initView( player );
         this.view.animateIdle();
-    }
-    
+      }    
+
+      app.stage.on('click', function() {
+        that.view.animateSaltoAlto();        
+    });
 };
